b4e835481731dbcc043e665881f6b4dc88f29ad2
Chris Rabe <chris.rabe14@gmail.com>
2021-07-14 18:23:55 +1000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Weather from "./components/Weather";
 
-function App() {
+const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <Weather />
+    </MainContainer>
   );
-}
+};
 
 export default App;
